ee36b24e9b733c166872c77b15ea55d18a02e0d7
Julian Tescher <virulent@gmail.com>
2012-10-06 11:46:02 -0700

    Refactor shape module for smaller method size

--- lib/descriptive-statistics/shape.rb
+++ lib/descriptive-statistics/shape.rb
@@ -1,19 +1,35 @@
 class DescriptiveStatistics
   module Shape
+
     def skewness
-      return if length < 1
+      return if length == 0
       return 0 if length == 1
-      sum_cubed_deviation = inject(0) {|sum, value| sum + (value - mean) ** 3}
-      cubed_standard_deviation = standard_deviation ** 3
       sum_cubed_deviation / ((length - 1) * cubed_standard_deviation.to_f)
     end
 
     def kurtosis
-      return if length < 1
+      return if length == 0
       return 0 if length == 1
-      sum_quarted_deviation = inject(0) {|sum, value| sum + (value - mean) ** 4}
-      quarted_standard_deviation = standard_deviation ** 4
       sum_quarted_deviation / ((length - 1) * quarted_standard_deviation.to_f)
     end
+
+  private
+
+    def sum_cubed_deviation
+      inject(0) {|sum, value| sum + (value - mean) ** 3}
+    end
+
+    def cubed_standard_deviation
+      standard_deviation ** 3
+    end
+
+    def sum_quarted_deviation
+      inject(0) {|sum, value| sum + (value - mean) ** 4}
+    end
+
+    def quarted_standard_deviation
+      standard_deviation ** 4
+    end
+
   end
 end
